3dcc1a5ee0b4ee0a5e90347b8f746a439571eacb
akatuki06 <akatuki0623@gmail.com>
2020-01-07 00:48:36 +0900

    test GET api/customers

---
+++ tests/Feature/ReportTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class ReportTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function api_customersにGETメソッドでアクセスできる()
+    {
+        // 実行
+        $response = $this->get('api/customers');
+        // 検証
+        $response->assertStatus(200);
+    }
+}
