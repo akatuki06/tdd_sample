d46e3d120d0b75fadabbb6a2e7103c0e6a7b56e4
akatuki06 <akatuki0623@gmail.com>
2020-01-08 01:19:14 +0900

    ide-helper:modelsの実行

--- app/Customer.php
+++ app/Customer.php
@@ -4,6 +4,16 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Customer
+ *
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Report[] $reports
+ * @property-read int|null $reports_count
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Customer newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Customer newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Customer query()
+ * @mixin \Eloquent
+ */
 class Customer extends Model
 {
     public function reports()
