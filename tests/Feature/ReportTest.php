5f649f765a167b23ccc29e332a52cd5c39b46dd2
akatuki06 <akatuki0623@gmail.com>
2020-01-15 00:29:28 +0900

    test追加 : POST api/customers

--- tests/Feature/ReportTest.php
+++ tests/Feature/ReportTest.php
@@ -56,8 +56,40 @@ class ReportTest extends TestCase
      */
     public function api_customersにPOSTメソッドでアクセスできる()
     {
-        $response = $this->post('api/customers');
+        $customer = [
+            'name' => 'customer_name',
+        ];
+        $response = $this->postJson('api/customers', $customer);
         $response->assertStatus(200);
+    }
+    /**
+     * @test
+     */
+    public function api_customersに顧客名をPOSTするとcustomersテーブルにそのデータが追加される()
+    {
+        $params = [
+            'name' => '顧客名',
+        ];
+        $this->postJson('api/customers', $params);
+        $this->assertDatabaseHas('customers', $params);
+    }
+    /**
+     * @test
+     */
+    public function POST_api_customersにnameが含まれない場合422UnprocessableEntityが返される()
+    {
+        $params = [];
+        $response = $this->postJson('api/customers', $params);
+        $response->assertStatus(\Illuminate\Http\Response::HTTP_UNPROCESSABLE_ENTITY);
+    }
+    /**
+     * @test
+     */
+    public function POST_api_customersのnameが空の場合422UnprocessableEntityが返される()
+    {
+        $params = ['name' => ''];
+        $response = $this->postJson('api/customers', $params);
+        $response->assertStatus(\Illuminate\Http\Response::HTTP_UNPROCESSABLE_ENTITY);
     }
     /**
      * @test
