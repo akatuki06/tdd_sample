a4e28f0eeb5079280ddcf6feeca88b09515aecbb
akatuki06 <akatuki0623@gmail.com>
2020-01-15 23:36:50 +0900

    Laravelのvalidationを使用

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -14,12 +14,7 @@ class ApiController extends Controller
 
     public function postCustomer(Request $request)
     {
-        if (!$request->json('name')) {
-            return response()->json(
-                [],
-                \Illuminate\Http\Response::HTTP_UNPROCESSABLE_ENTITY
-            );
-        }
+        $this->validate($request, ['name' => 'required']);
         $customer = new \App\Customer();
         $customer->name = $request->json('name');
         $customer->save();
